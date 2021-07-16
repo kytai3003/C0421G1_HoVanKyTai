a775577ef91ce949cec4ed52a3bf618473bacd25
kytai3003 <tai.hvk@gmail.com>
2021-07-16 15:47:37 +0700

    bai tap va thuc hanh database overview

--- module_03/01_database_introduction/exercise/create_other_table_by_statement.sql
+++ module_03/01_database_introduction/exercise/create_other_table_by_statement.sql
@@ -1,17 +1,19 @@
 create database student_management;
 create table student_management.student(
-id INT not null,
+id INT not null auto_increment,
 `name` varchar(45) null,
 age int null,
 country varchar(45) null,
 primary key (id));
 create table student_management.class(
-id int not null,
+id int not null auto_increment,
 `name` varchar(45) null,
 primary key (id));
 create table student_management.teacher(
-id INT not null,
+id INT not null auto_increment,
 `name` varchar(45) null,
 age int null,
 country varchar(45) null,
 primary key (id));
+select *
+from teacher;

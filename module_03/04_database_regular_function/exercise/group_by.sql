ff5a244903171613d2f64b6fe04c268ee072d4ca
kytai3003 <tai.hvk@gmail.com>
2021-07-23 13:35:26 +0700

    casestudy

--- module_03/04_database_regular_function/exercise/group_by.sql
+++ module_03/04_database_regular_function/exercise/group_by.sql
@@ -5,11 +5,11 @@ select *, max(`subject`.credit) as 'Credit lớn nhất'
 from `subject`;
 
 -- Hiển thị các thông tin môn học có điểm thi lớn nhất.
-select s.SubId, s.SubName, s.Credit, max(m.mark) as 'Điểm thi lớn nhất'
-from `subject` s inner join Mark m on m.SubId = s.SubId;
+select s.sub_id, s.sub_name, s.credit, max(m.mark) as 'Điểm thi lớn nhất'
+from `subject` s inner join mark m on m.sub_id = s.sub_id;
 
 -- Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần
-select s.StudentId, s.StudentName, s.Address, s.Phone, m.mark, avg(m.mark) as 'DTB'
-from student s inner join Mark m on m.StudentId = s.StudentId
-group by s.StudentId
+select s.student_id, s.student_name, s.address, s.phone, m.mark, avg(m.mark) as 'DTB'
+from student s inner join Mark m on m.student_id = s.student_id
+group by s.student_id
 order by DTB desc;
--- module_03/database_casestudy/query.sql
+++ module_03/database_casestudy/query.sql
@@ -66,7 +66,7 @@ from khach_hang;
 -- Cách 2:
 select ho_ten
 from khach_hang
-group by id_khach_hang; 
+group by ho_ten; 
 -- Cách 3:
 
 -- 9.Thực hiện thống kê doanh thu theo tháng, nghĩa là tương ứng với mỗi tháng trong năm 2019 
@@ -116,5 +116,97 @@ group by d.id_dich_vu;
 
 -- 13.	Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các Khách hàng đã đặt phòng. 
 -- (Lưu ý là có thể có nhiều dịch vụ có số lần sử dụng nhiều như nhau).
-		
+select dv.id_dich_vu_di_kem, dv.ten_dich_vu_di_kem, dv.gia, dv.don_vi,
+count(dv.id_dich_vu_di_kem) as 'so_lan'
+from dich_vu_di_kem dv 
+inner join hop_dong_chi_tiet hd on dv.id_dich_vu_di_kem = hd.id_dich_vu_di_kem
+inner join hop_dong h on h.id_hop_dong = hd.id_hop_dong
+group by dv.ten_dich_vu_di_kem
+having so_lan >= max(so_lan)
+order by so_lan desc;
 
+-- 14.	Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một lần duy nhất.
+--  Thông tin hiển thị bao gồm IDHopDong, TenLoaiDichVu, TenDichVuDiKem, SoLanSuDung.
+select h.id_hop_dong, l.ten_loai_dich_vu, dv.ten_dich_vu_di_kem, 
+count(dv.id_dich_vu_di_kem) as 'so_lan_su_dung'	
+from dich_vu d inner join loai_dich_vu l on l.id_loai_dich_vu = d.id_loai_dich_vu
+inner join hop_dong h on d.id_dich_vu = h.id_hop_dong
+inner join hop_dong_chi_tiet hd on hd.id_hop_dong = h.id_hop_dong
+inner join dich_vu_di_kem dv on dv.id_dich_vu_di_kem = hd.id_dich_vu_di_kem
+group by dv.ten_dich_vu_di_kem
+having so_lan_su_dung = 1;
+
+-- 15.	Hiển thi thông tin của tất cả nhân viên bao gồm IDNhanVien, HoTen, TrinhDo, TenBoPhan, 
+-- SoDienThoai, DiaChi mới chỉ lập được tối đa 3 hợp đồng từ năm 2018 đến 2019.
+select n.id_nhan_vien, n.ho_ten, t.trinh_do, b.ten_bo_phan,
+count(h.id_nhan_vien) as 'so_lan_lap_hd'
+from nhan_vien n inner join hop_dong h on h.id_nhan_vien = n.id_nhan_vien
+inner join trinh_do t on t.id_trinh_do = n.id_trinh_do
+inner join bo_phan b on b.id_bo_phan = n.id_bo_phan
+group by h.id_nhan_vien
+having so_lan_lap_hd between 0 and 3;
+
+-- 16.	Xóa những Nhân viên chưa từng lập được hợp đồng nào từ năm 2017 đến năm 2019.
+alter table nhan_vien
+add constraint fk_vi_tri foreign key (id_vi_tri) references vi_tri(id_vi_tri) on delete cascade,
+add constraint fk_trinh_do foreign key (id_trinh_do) references trinh_do(id_trinh_do) on delete cascade,
+add constraint fk_bo_phan foreign key (id_bo_phan) references bo_phan(id_bo_phan) on delete cascade;
+set SQL_SAFE_UPDATES = 0;
+delete 
+from nhan_vien
+where not exists (
+select * 
+from hop_dong  
+where hop_dong.id_nhan_vien = nhan_vien.id_nhan_vien
+);
+set SQL_SAFE_UPDATES = 1;
+
+-- 17.	Cập nhật thông tin những khách hàng có TenLoaiKhachHang từ  Platinium lên Diamond,
+--  chỉ cập nhật những khách hàng đã từng đặt phòng với tổng Tiền thanh toán 
+--  trong năm 2019 là lớn hơn 10.000.000 VNĐ.
+update khach_hang 
+set id_loai_khach = 1 
+where id_khach_hang = 2 and exists(
+select *, count(d.chi_phi_thue) as 'tong_tien'
+from khach_hang k inner join hop_dong h on h.id_khach_hang = k.id_khach_hang
+inner join dich_vu d on d.id_dich_vu = h.id_dich_vu
+where year(h.ngay_lam_hop_dong) = 2019
+group by k.id_khach_hang
+having tong_tien > 10000000
+);
+
+-- 18.	Xóa những khách hàng có hợp đồng trước năm 2016 (chú ý ràng buộc giữa các bảng).
+alter table hop_dong
+add constraint fk_khach_hang foreign key (id_khach_hang) references khach_hang(id_khach_hang) on delete cascade;
+alter table khach_hang
+add constraint fk_loai_khach foreign key (id_loai_khach) references loai_khach(id_loai_khach) on delete cascade;
+set SQL_SAFE_UPDATES = 0;
+delete 
+from khach_hang 
+where id_khach_hang in (
+select id_khach_hang 
+from hop_dong
+where year(hop_dong.ngay_lam_hop_dong) < 2016
+);
+set SQL_SAFE_UPDATES = 1;
+
+-- 19.	Cập nhật giá cho các Dịch vụ đi kèm được sử dụng trên 10 lần trong năm 2019 lên gấp đôi.
+update dich_vu_di_kem
+set gia = gia*2
+where exists (
+select *,
+count(hd.id_dich_vu_di_kem) as 'so_lan'
+from hop_dong_chi_tiet hd inner join dich_vu_di_kem dv on dv.id_dich_vu_di_kem = hd.id_dich_vu_di_kem
+inner join hop_dong h on h.id_hop_dong = hd.id_hop_dong
+where year(h.ngay_lam_hop_dong) = '2019'
+group by dv.id_dich_vu_di_kem
+having so_lan > 10
+);
+
+-- 20.	Hiển thị thông tin của tất cả các Nhân viên và Khách hàng có trong hệ thống, 
+-- thông tin hiển thị bao gồm ID (IDNhanVien, IDKhachHang), HoTen, Email, SoDienThoai, NgaySinh, DiaChi.
+select n.id_nhan_vien as 'id' , n.ho_ten, n.email, n.so_dien_thoai, n.ngay_sinh, n.dia_chi 
+from nhan_vien n
+union all
+select k.id_khach_hang, k.ho_ten, k.email, k.so_dien_thoai, k.ngay_sinh, k.dia_chi 
+from khach_hang k

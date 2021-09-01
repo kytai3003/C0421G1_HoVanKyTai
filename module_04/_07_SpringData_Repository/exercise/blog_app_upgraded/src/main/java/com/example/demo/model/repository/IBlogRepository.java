891b8be0d9a5232ef4b2614746853302cee6d939
kytai3003 <tai.hvk@gmail.com>
2021-09-01 11:12:14 +0700

    Spring JPA Repository

---
+++ module_04/_07_SpringData_Repository/exercise/blog_app_upgraded/src/main/java/com/example/demo/model/repository/IBlogRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo.model.repository;
+
+import com.example.demo.model.bean.Blog;
+import com.example.demo.model.bean.Catelogy;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface IBlogRepository extends JpaRepository<Blog, Integer> {
+    List<Blog> findByBlogTitleContaining(String title);
+
+    List<Blog> findAllByCatelogyCatelogyId(int id);
+}
